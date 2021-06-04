22e38c5a30008979a3f8b7bfb4c7b48454e9d30f
action@github.com <GitHub Action>
2021-06-04 20:13:37 +0900

    deploy.js による自動コミット

--- deploy.js
+++ deploy.js
@@ -8,7 +8,10 @@ const main = async () => {
 
     await run`git add .`
     const files = (await invoke`git status --short`).split("\n").filter(x => x.length !== 0)
-    if (files.length === 0) { return }
+    if (files.length === 0) {
+        console.log("変更が無かったので自動コミットは実行されません")
+        return
+    }
 
     await run`git -c user.name=action@github.com -c user.email="GitHub Action" commit --all --message "${path.relative(process.cwd(), __filename)} による自動コミット"`
     await run`git push`
