0e51a1ec13c4682c5c5b7ca49392e212e06f1f4e
action@github.com <GitHub Action>
2021-06-04 20:11:52 +0900

    deploy.js による自動コミット

--- deploy.js
+++ deploy.js
@@ -1,54 +1,13 @@
 //@ts-check
-const { spawn, exec, execFileSync, execSync } = require("child_process")
+const { run, invoke } = require("./shell")
 const path = require("path")
-
-/**
- * @param {string} command
- * @returns {Promise<{ signal: string, output: string }>}
- */
- const runCore = (command, { inheritStdio }) => new Promise((onSuccess, onError) => {
-    console.log(`> ${command}`)
-    const child = spawn(command, [], { shell: true, stdio: inheritStdio ? "inherit" : undefined })
-    /** @type {string[]} */
-    const output = []
-    child.stdout?.on("data", chunk => output.push(chunk.toString()))
-    child.stderr?.on("data", chunk => output.push(chunk.toString()))
-    child.on("error", onError)
-    child.on("exit", (exitCode, signal) => {
-        if (exitCode !== 0) {
-            onError(new (class ExitError extends Error {
-                constructor() { super("A non-zero exit code was returned.") }
-                exitCode = exitCode
-                signal = signal
-            }))
-        }
-        else {
-            onSuccess({
-                signal,
-                output: output.join(""),
-            })
-        }
-    })
-})
-/**
- * @param {TemplateStringsArray} command
- * @param  {...unknown} args
- * @returns {Promise<{ signal: string }>}
- */
-const run = (command, ...args) => runCore(String.raw(command, args), { inheritStdio: true })
-/**
- * @param {TemplateStringsArray} command
- * @param  {...unknown} args
- * @returns {Promise<string>}
- */
-const invoke = async (command, ...args) => (await runCore(String.raw(command, args), { inheritStdio: false })).output
 
 const main = async () => {
     await run`npm install`
     await run`npm run build`
 
     await run`git add .`
-    const files = (await invoke`git status --short`).trim().split("\n")
+    const files = (await invoke`git status --short`).split("\n").filter(x => x.length !== 0)
     if (files.length === 0) { return }
 
     await run`git -c user.name=action@github.com -c user.email="GitHub Action" commit --all --message "${path.relative(process.cwd(), __filename)} による自動コミット"`
---
+++ shell.js
@@ -0,0 +1,65 @@
+//@ts-check
+const { spawn } = require("child_process")
+
+/**
+ * @param {string} command
+ * @param {{ inheritStdio: boolean }} _arg1
+ * @returns {Promise<{ signal: string, output: string }>}
+ */
+ const runCore = (command, { inheritStdio }) => new Promise((onSuccess, onError) => {
+    console.log(`> ${command}`)
+    const child = spawn(command, [], { shell: true, stdio: inheritStdio ? "inherit" : undefined })
+    /** @type {string[]} */
+    const output = []
+
+    /**
+     * @template T, TargetType
+     * @typedef { { [k in keyof T]: T[k] extends TargetType ? k : never }[keyof T] } FilterKeys
+     */
+    /**
+     * @typedef {
+        & FilterKeys<typeof process, import("stream").Writable>
+        & FilterKeys<typeof child, import("stream").Readable | undefined>
+    } WriterKey
+     */
+    const addOnDataListener = (/** @type {WriterKey} */ ioKey) => child[ioKey]?.on("data", chunk => {
+        const data = chunk.toString()
+        process[ioKey].write(data)
+        output.push(data)
+    })
+
+    addOnDataListener("stdout")
+    addOnDataListener("stderr")
+    child.on("error", onError)
+    child.on("exit", (exitCode, signal) => {
+        if (exitCode !== 0) {
+            onError(new (class ExitError extends Error {
+                constructor() { super("A non-zero exit code was returned.") }
+                exitCode = exitCode
+                signal = signal
+                output = output.join("")
+            }))
+        }
+        else {
+            onSuccess({
+                signal,
+                output: output.join(""),
+            })
+        }
+    })
+})
+/**
+ * @param {TemplateStringsArray} command
+ * @param  {...unknown} args
+ * @returns {Promise<{ signal: string }>}
+ */
+const run = (command, ...args) => runCore(String.raw(command, args), { inheritStdio: true })
+exports.run = run
+
+/**
+ * @param {TemplateStringsArray} command
+ * @param  {...unknown} args
+ * @returns {Promise<string>}
+ */
+const invoke = async (command, ...args) => (await runCore(String.raw(command, args), { inheritStdio: false })).output
+exports.invoke = invoke
