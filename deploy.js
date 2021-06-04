38cf31765b6677421deea0c6622b539dbdf2e88f
action@github.com <GitHub Action>
2021-06-04 12:17:42 +0900

    deploy.js による自動コミット

--- deploy.js
+++ deploy.js
@@ -1,5 +1,6 @@
 //@ts-check
 const { spawnSync } = require("child_process")
+const path = require("path")
 const run = (/** @type {string} */ arg0, /** @type {string[]} */ ...args) => {
     console.log(`> ${arg0}${args.map(x => ` ${x}`).join("")}`)
     const result = spawnSync(arg0, args, { shell: true, stdio: "inherit", encoding: "utf-8" })
@@ -10,5 +11,5 @@ run("npm", "install")
 run("npm", "run", "build")
 
 run("git", "add", ".")
-run("git", "-c", `user.name=action@github.com`, "-c", `user.email="GitHub Action"`, "commit", "--all", "-m", `"${__filename} による自動コミット"`)
+run("git", "-c", `user.name=action@github.com`, "-c", `user.email="GitHub Action"`, "commit", "--all", "-m", `"${path.relative(process.cwd(), __filename)} による自動コミット"`)
 run("git", "push")
